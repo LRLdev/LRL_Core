35787f74ebfa56352f958083ea637d4265f41abb
Lucas Leite <raphaleite.2002@gmail.com>
2022-07-06 22:49:37 -0300

    Get User Data

--- src/server/functions/essentials.lua
+++ src/server/functions/essentials.lua
@@ -1,15 +1,16 @@
 function LRL.Server.GetUserId(source)
-    assert(user_id, '[LRL] Source não definido')
-    return LRL.playersIds[source]
+    assert(source, '[LRL] Source não definido')
+    return LRL.Server.playersIds[source]
 end
 
 function LRL.Server.GetUserSource(user_id)
     assert(user_id, '[LRL] User_id não definido')
-    return LRL.playersSource[user_id]
+    return LRL.Server.playersSource[user_id]
 end
 
 function LRL.Server.GetUserData(user_id, key)
-    local user_id = parseInt(user_id)
+    assert(user_id, '[LRL] User_id não definido')
+    assert(key, '[LRL] Key não definido')
     local result = MySQL.single.await(queries[framework]['get_user_data'],{ user_id = user_id, key = key })
     
     if result then
@@ -23,12 +24,15 @@ end
 -- @param user_id The user id.
 -- @returns The user datatable.
 function LRL.Server.GetUserDatatable(user_id)
-    local user_id = parseInt(user_id)
-    local result = MySQL.single.await(queries['bahamas']['get_user_datatable'],{ user_id })
     
-    if result then
-        return result
+    local result = MySQL.single.await(queries[framework]['get_user_datatable'],{ user_id = 1 })
+    
+    local datatable = {}
+    for k,v in pairs(json.decode(result["dvalue"])) do
+        datatable[k] = v
     end
+
+    return datatable
 end
 
 function LRL.Server.GetServerData(key)
