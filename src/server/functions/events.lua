03cae4ed1ddb9f13ab65dcedabd8265e79cbfcca
Lucas Leite <raphaleite.2002@gmail.com>
2022-07-06 22:50:11 -0300

    Event Handler

--- src/server/functions/events.lua
+++ src/server/functions/events.lua
@@ -2,12 +2,13 @@ LRL.Server.playersSource = {}
 LRL.Server.playersIds = {}
 
 AddEventHandler("vRP:playerSpawn",function(user_id,source)
-    print(user_id)
-    print(source)
-
-    print("TESTE")
-    print(json.encode(LRL.Server.playersSource))
-    print(json.encode(LRL.Server.playersIds))
     LRL.Server.playersSource[user_id] = source
     LRL.Server.playersIds[source] = user_id
+end)
+
+AddEventHandler("playerDropped",function(reason)
+    local source = source
+    local user_id = LRL.Server.playersIds[source]
+    LRL.Server.playersSource[user_id] = nil
+    LRL.Server.playersIds[source] = nil
 end)
