bcfab525000f2c804d0ced514c481d1b787f37ea
Lucas Leite <raphaleite.2002@gmail.com>
2022-07-12 22:29:13 -0300

    [ADD] Adding new queries

--- src/server/core/queries.lua
+++ src/server/core/queries.lua
@@ -4,6 +4,9 @@ queries = {
         ['get_identity'] = 'SELECT * FROM summerz_characters WHERE id = ?',
         ['get_fines'] = 'SELECT fines AS multas FROM summerz_characters WHERE id = @id',
         ['set_fines'] = 'UPDATE summerz_characters SET fines = fines + @fines WHERE id = @id',
+        ['add_prison'] = 'UPDATE summerz_characters SET prison = @prison WHERE id = @user_id',
+        ['add_user_money'] = 'UPDATE summerz_characters SET bank = bank + @bank WHERE id = @id',
+        ['get_user_money'] = 'SELECT bank FROM summerz_characters WHERE id = @id',
         ['get_user_datatable'] = 'SELECT dvalue FROM summerz_playerdata WHERE user_id = @user_id AND dkey = "Datatable"',
         ['get_user_data'] = 'SELECT dvalue FROM summerz_playerdata WHERE user_id = @user_id AND dkey = @key',
         ['get_server_data'] = 'SELECT dvalue FROM summerz_entitydata WHERE dkey = @dkey',
@@ -13,6 +16,23 @@ queries = {
         ['get_identity'] = 'SELECT * FROM vrp_user_identities WHERE user_id = ?',
         ['get_fines'] = 'SELECT dvalue FROM vrp_user_data WHERE user_id = @user_id AND dkey = "vRP:multas"',
         ['set_fines'] = 'REPLACE INTO vrp_user_data(user_id,dkey,dvalue) VALUES(@id,"vRP:multas",@fines)',
+        ['add_user_money'] = 'UPDATE vrp_user_moneys SET bank = bank + @bank WHERE id = @id',
+        ['get_user_money'] = "SELECT bank FROM vrp_user_moneys WHERE user_id = @id",
         ['get_user_datatable'] = 'SELECT dvalue FROM vrp_user_data WHERE user_id = @user_id AND dkey = "vRP:datatable"',
-    } 
+        ['set_user_data'] = 'REPLACE INTO vrp_user_data(user_id,dkey,dvalue) VALUES(@user_id,@key,@value)'
+    },
+    ['crv5'] = {
+        ['get_identity'] = 'SELECT * FROM summerz_characters WHERE id = ?',
+        ['get_fines'] = 'SELECT fines AS multas FROM summerz_characters WHERE id = @id',
+        ['set_fines'] = 'UPDATE summerz_characters SET fines = fines + @fines WHERE id = @id',
+        ['add_prison'] = 'UPDATE summerz_characters SET prison = @prison WHERE id = @user_id',
+        ['add_user_money'] = 'UPDATE summerz_bank SET value = value + @bank WHERE user_id = @id AND mode = "Private" AND owner = 1',
+        ['del_user_money'] = 'UPDATE summerz_bank SET value = value - @bank WHERE user_id = @id AND mode = "Private" AND owner = 1',
+        ['get_user_money'] = 'SELECT bank FROM summerz_characters WHERE id = @id',
+        ['get_user_datatable'] = 'SELECT dvalue FROM summerz_playerdata WHERE user_id = @user_id AND dkey = "Datatable"',
+        ['get_user_data'] = 'SELECT dvalue FROM summerz_playerdata WHERE user_id = @user_id AND dkey = @key',
+        ['set_user_data'] = 'REPLACE INTO summerz_playerdata(user_id,dkey,dvalue) VALUES(@user_id,@key,@value)',
+        ['get_server_data'] = 'SELECT dvalue FROM summerz_entitydata WHERE dkey = @dkey',
+        ['set_server_data'] = 'REPLACE INTO summerz_entitydata(dkey,dvalue) VALUES(@dkey,@value)'
+    },
 }
