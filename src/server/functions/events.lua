5543b68be23cc2ce8524b1d405806d7692fe843f
Lucas Leite <raphaleite.2002@gmail.com>
2022-07-12 22:37:41 -0300

    [ADD] Adding playerConnect event

--- src/server/functions/events.lua
+++ src/server/functions/events.lua
@@ -6,6 +6,13 @@ AddEventHandler("vRP:playerSpawn",function(user_id,source)
     LRL.Server.playersIds[source] = user_id
 end)
 
+-- Event to handle a creative v5 player connection
+AddEventHandler("playerConnect",function(user_id,source)
+    LRL.Server.playersSource[user_id] = source
+    LRL.Server.playersIds[source] = user_id
+end)
+
+
 AddEventHandler("playerDropped",function(reason)
     local source = source
     local user_id = LRL.Server.playersIds[source]
